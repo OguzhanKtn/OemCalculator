dfb587c850d39bd6c12c98ac9d760d5b61ed6ac0
Oğuzhan <81297977+OguzhanKtn@users.noreply.github.com>
2023-03-22 21:01:46 +0300

    oem

--- app.js
+++ app.js
@@ -23,16 +23,68 @@ const ProductController =(function(){
         totalPrice:0
     }
 
+    return{
+        getProducts : function(){
+            return data.products;
+        },
+        getData : function(){
+            return data;
+        }
+    }
+
 })();
 
 // UI Controller
 const UIController =(function(){
 
+    const Selectors = {
+        productList : "#item-list",
+        addButton : ".addBtn",
+        productName : "#productName",
+        productPrice : "#productPrice"
+    }
+
+    return{
+        createProductList : function(products){
+            let html ="";
+
+            products.forEach(prd => {
+                html += `
+                    <tr>
+                        <td>${prd.id}</td>
+                        <td>${prd.name}</td>
+                        <td>${prd.price} $</td>
+                        <td class="text-right">
+                            <button type="submit" class="btn btn-warning btn-sm">
+                                <i class="far fa-edit"></i>
+                            </button>
+                        </td>
+                    </tr>
+                
+                `;
+            });
+
+            document.querySelector(Selectors.productList).innerHTML = html;
+        },
+        getSelectors : function(){
+            return Selectors;
+        }
+    }
 })();
 
 // App Controller
-const AppController =(function(ProductCtrl,UICtrl){
+const App =(function(ProductCtrl,UICtrl){
 
-    
+    const UISelectors = UIController.getSelectors();
+
+    return{
+        init : function(){
+            const products = ProductCtrl.getProducts();
+
+            UICtrl.createProductList(products);
+        }
+    }
 
 })(ProductController,UIController);
+
+App.init();
